91f5adbb030181b43925f3f725b1aa5efddaa1f5
Jesper Sultan <jesper.sultan@gmail.com>
2016-10-27 13:37:00 +0200

    Import responsive menus from other assets

--- src/scripts/subMenu.js
+++ src/scripts/subMenu.js
@@ -7,25 +7,35 @@
     /**
      * Fix half pixel bug on sub menus
      * caused by css transforms using % values
-    */
+     */
+
     const height = subMenu.outerHeight();
     const width = subMenu.outerWidth();
+
     if (height & 1) {
-      subMenu.outerHeight(height + 1);
+      subMenu.outerHeight(parseInt(height + 1, 10));
     }
     if (width & 1) {
-      subMenu.outerWidth(width + 1);
+      subMenu.outerWidth(parseInt(width + 1, 10));
     }
 
     /**
-     *  Add a show/hide toggle for sub menus
+     * Add a show/hide toggle for sub menus
      *
      */
 
     $('.menu-item-has-children').append('<span class="toggle-sub-menu"></span>')
 
-    $('.toggle-sub-menu').click((e) => {
-      $(e.currentTarget).parent().children('ul.sub-menu').toggleClass('is-open');
+    $('.toggle-sub-menu').on('click', (e) => {
+
+      // Sub-menu offset from viewport top
+      const top = e.target.parentNode.getBoundingClientRect().top;
+
+      $(e.currentTarget).parent().parent().toggleClass('sub-menu-is-open');
+      $(e.currentTarget).parent().toggleClass('sub-menu-is-open');
+      $(e.currentTarget).parent().children('ul.sub-menu').toggleClass('is-open').css('top', 70 - top);
+      $(e.currentTarget).toggleClass('is-open');
+
     });
 
   });
