60221def2e7303afe1e6cb5fc281586641936be0
Jesper Sultan <jesper.sultan@gmail.com>
2016-10-28 14:34:26 +0200

    Rename default page template to page

--- src/templates/page.php
+++ src/templates/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main class="main default-page" role="main">
+<main class="main page" role="main">
 
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php the_content(); ?>
